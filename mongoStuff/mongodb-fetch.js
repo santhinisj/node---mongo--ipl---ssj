aff55c5dc972b003894f9e9ee765c47238658d78
Santhini S <santhinisj@gmail.com>
2018-07-11 17:18:05 +0530

    Getting data from users collection in ipldataset DB.
    - find() gets all docs from users collection
    - convert them to array
    - toArray returns promise,hence .then => print docs
    - no db.close() , it interferes with code

---
+++ mongoStuff/mongodb-fetch.js
@@ -0,0 +1,19 @@
+/*jshint esversion:6 */
+// const MongoClient = require("mongodb").MongoClient;
+// const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    if (err) {
+        return console.log("Connection to MongoDB server refused");
+    }
+    const db = client.db('ipldataset');
+    console.log('Connected to mongodb database');
+
+    db.collection('Users').find().toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+        console.log("User Data");
+    }), (err) => {
+        console.log('Unable to fetch data', err);
+    };
+
+});
