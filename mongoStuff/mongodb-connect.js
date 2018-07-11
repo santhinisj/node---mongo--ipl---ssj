271521fdc3bc554a3a20088f4eb3ef8bb12d1130
Santhini S <santhinisj@gmail.com>
2018-07-11 15:35:23 +0530

    ES6 destructuring
    create new variables from object properties.

--- mongoStuff/mongodb-connect.js
+++ mongoStuff/mongodb-connect.js
@@ -1,5 +1,11 @@
 /*jshint esversion:6 */
 const MongoClient = require("mongodb").MongoClient;
+
+
+var user = { name: 'santhi', location: 'tvpm' };
+var { name } = user;
+console.log(name);
+
 MongoClient.connect('mongodb://localhost:27017/ipldataset', (err, client) => {
     if (err) {
         return console.log("Connection to MongoDB server refused");
@@ -7,32 +13,4 @@ MongoClient.connect('mongodb://localhost:27017/ipldataset', (err, client) => {
     //reference for ipldataset DB.
     const db = client.db('ipldataset');
     console.log('Connected to mongodb database');
-    db.collection('ipldataset').insertOne({
-        text: 'Adding text here for demo',
-        completed: false
-            //result is fired if things go well
-    }, (err, result) => {
-        if (err) {
-            return console.log("unable to insert demo text", err);
-
-        }
-        //ops contain all the documents inserted. Here one!
-        //undefined is for the filter fn and indendation is 2.
-        console.log(JSON.stringify(result.ops), undefined, 2);
-
-    });
-
-    // create new collection - user - name, age , location
-    db.collection('Users').insertOne({
-        name: 'santhi',
-        age: '5',
-        location: 'kerala'
-    }, (err, result) => {
-        if (err) {
-            return console.log("Unable to connect");
-        }
-        console.log(JSON.stringify(result.ops[0]._id.getTimestamp()), undefined, 2);
-    });
-
-    client.close();
 });
